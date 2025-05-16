9e854f6a2795be8ffec66d9b9433870c28d66dc7
LocianoBenjamin <diplomacytoday@myyahoo.com>
2025-05-16 12:26:40 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+function linkedin() {
+    window.open('https://www.linkedin.com/in/lociano-benjamin-9532303b')
+}
+
+function instagram() {
+    window.open("https://www.instagram.com/benjaminlociano/")
+}
+
+function x() {
+    window.open("https://twitter.com/LocianoB")
+}
+
+function email() {
+    window.open("mailto:diplomacytoday#myyahoo.com")
+}
+
+function buyfive() {
+    window.open("https://www.amazon.com/500-Years-Exploitation-Lociano-Benjamin/dp/1615662596/ref=sr_1_4?dib=eyJ2IjoiMSJ9.Z_NyNVXVE3q3ZTmtvPPTtgS0atPUHNXgHlzx6x-z3WF4moa21HOXkf5ycq9bmRSD.jHdlp8q9sSVEpJ3-XnHPmbcKTCcMHPQ_3zMswD29V1w&dib_tag=se&qid=1746798549&refinements=p_27%3ALociano+Benjamin&s=books&sr=1-4&text=Lociano+Benjamin")
+}
+
+function buypoet() {
+    window.open("https://www.amazon.com/Perilous-Life-Lociano-Benjamin/dp/173767890X/ref=tmm_pap_swatch_0?_encoding=UTF8&dib_tag=se&dib=eyJ2IjoiMSJ9.Z_NyNVXVE3q3ZTmtvPPTtgS0atPUHNXgHlzx6x-z3WF4moa21HOXkf5ycq9bmRSD.jHdlp8q9sSVEpJ3-XnHPmbcKTCcMHPQ_3zMswD29V1w&qid=1746798610&sr=1-2")
+}
+
+function sidenav() {
+    document.getElementById("sidenav").style.display = "block"
+    document.getElementById("sidenav").style.animation = "slidein 0.5s linear"
+}
+
+function closenav() {
+    document.getElementById("sidenav").style.display = "none"
+}
